72fc246f2e4257edadd1b7011a2a15f8445f61fd
Mosmain <mosmain@yandex.ru>
2024-12-19 02:23:58 +0300

    fix: replase to monke

--- components/GsapExample.vue
+++ components/GsapExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-fit">
-    <h1 ref="title" class="text-3xl font-bold mb-4">🐔PETUCH🐔</h1>
+    <h1 ref="title" class="text-3xl font-bold mb-4">🐵MONKE🐵</h1>
     <button class="bg-blue-500 text-white px-4 py-2 rounded" @click="animate">
       Animate!
     </button>
